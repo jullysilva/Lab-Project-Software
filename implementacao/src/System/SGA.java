967346fe62bdd9265d1db614721615e5d6f27e1d
Jully Ketely Alves Da Silva <1181631@sga.pucminas.br>
2022-08-17 20:03:29 -0300

    Create project of implementation of Java

---
+++ implementacao/src/System/SGA.java
@@ -0,0 +1,9 @@
+package System;
+
+public class SGA {
+
+	public static void main(String[] args) {
+		System.out.println("----------------------- SISTEMA DE MATRICULA -----------------------\n");
+	}
+
+}
