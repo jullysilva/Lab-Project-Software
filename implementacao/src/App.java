dcaa70cf9979009dae5614b3204d4cb720446b93
Jully Silva <jkasilva@sga.pucminas.br>
2022-08-27 21:56:19 -0300

    criacao de painel principal do sistema

--- implementacao/src/App.java
+++ implementacao/src/App.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("PROJETO MATRÍCULA");
+    	Scanner scanner = new Scanner(System.in);
+        int TLogin = 1;
+    	
+    	System.out.println("PROJETO MATRÍCULA");
+        
+        do {
+        	System.out.print("\t 1 - ALUNO \n\t 2 - PROFESSOR \n\t 3 - ADMINISTRADOR \n\t 0 - ABANDONAR SISTEMA");
+        	System.out.print("\n\nEscolha: ");
+        	TLogin = scanner.nextInt();
+        	
+        	switch (TLogin) {
+        	case 1:
+        		loginAluno();
+        		break;
+        	case 2: 
+        		loginProfessor();
+        		break;
+        	case 3: 
+        		loginAdministrador();
+        		break;
+        	default:
+        		break;
+        	}
+        } while(TLogin != 0);
+        
+        scanner.close();
+    }
+
+    public static void loginAluno() {
+    	System.out.println("ACESSO ALUNO");
+    }
+    
+    public static void loginProfessor() {
+    	System.out.println("ACESSO PROFESSOR");
+    }
+    
+    public static void loginAdministrador() {
+    	System.out.println("ACESSO ADMINISTRADOR");
     }
 }
