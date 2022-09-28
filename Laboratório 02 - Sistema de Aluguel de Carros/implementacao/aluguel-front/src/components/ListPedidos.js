8d7394d9aca5ead5e5439e9cd38c11476cc0598b
Jully Silva <jkasilva@sga.pucminas.br>
2022-09-28 15:29:01 -0300

    ajustar listagem de pedidos

--- Laboratório 02 - Sistema de Aluguel de Carros/implementacao/aluguel-front/src/components/ListPedidos.js
+++ Laboratório 02 - Sistema de Aluguel de Carros/implementacao/aluguel-front/src/components/ListPedidos.js
@@ -1,58 +1,93 @@
-import React from "react";
+import React, { usePromise, useCallback } from "react";
+import BuscarTodosPedidos from "../services/Pedidos";
 
- const ListPedido = () => {
+const ListPedido = () => {
+  const buscarPedidos = useCallback(async () => {
+    return await BuscarTodosPedidos();
+  }, []);
+
+  // const data = buscarPedidos();
+
+  // console.log(data);
+  const data = [
+    {
+      id: "1",
+      concideracaoAgente: null,
+      parecer: null,
+      cliente: {
+        id: 1,
+        cpf: "string",
+        rg: "string",
+        nome: "string",
+        endereco: "string",
+        email: "string",
+        senha: "string",
+      },
+      agente: null,
+      automovel: {
+        matricula: "1111",
+        ano: 2000,
+        marca: "Honda",
+        modelo: "aaa",
+        placa: "AAA1111",
+      },
+      contrato: {
+        id: 1,
+        valor: null,
+        parcela: null,
+      },
+    },
+  ];
+
   return (
-    <div class="modal fade" data-toggle="modal" id="visualizar" role="dialog">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="demo"></h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="form-group">
-                <label>
-                  <h5>Gênero</h5>
-                </label>
-                <div id="demo1"></div>
-              </div>
-              <div class="form-group">
-                <label>
-                  <h5>Temporadas</h5>
-                </label>
-                <div id="demo2"></div>
-              </div>
-              <div class="form-group">
-                <label>
-                  <h5>Personagem Principal</h5>
-                </label>
-                <div id="demo3"></div>
-              </div>
-              <div class="form-group">
-                <label>
-                  <h5>Público</h5>
-                </label>
-                <div id="demo4"></div>
-              </div>
-              <div class="form-group">
-                <label>
-                  <h5>Sinopse</h5>
-                </label>
-                <div id="demo6"></div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <table>
+        <tr>
+          <th>Cliente</th>
+          <th>Agente</th>
+          <th>Automovel</th>
+          <th>Contrato</th>
+          <th>Parecer</th>
+          <th id="l">Alteração</th>
+        </tr>
+
+        {data.map((index, item, array) => (
+          <tr index={index}>
+            {console.log(array[item].cliente.nome)}
+            <td class="cursor">{array[item].cliente.nome}</td>
+            <td>{array[item].agente}</td>
+            <td>
+              {array[item].automovel.modelo}, {array[item].automovel.marca},{" "}
+              {array[item].automovel.placa},{array[item].automovel.ano}
+            </td>
+            <td>{array[item].parecer}</td>
+            <td>{array[item].concideracaoAgente}</td>
+            <td>
+              <a>
+                <button class="btn btn-warning" onclick="alterar({i})">
+                  Alterar
+                </button>
+              </a>{" "}
+              |{" "}
+              <a>
+                <button class="btn btn-danger" onclick="excluir(${i})">
+                  Cancelar pedido
+                </button>
+              </a>{" "}
+              |{" "}
+              <button
+                class="btn btn-info"
+                data-toggle="modal"
+                data-target="#visualizar"
+                onclick="preencheModal(${i})"
+              >
+                Ver mais
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </>
   );
 };
-export default ListPedido
+export default ListPedido;
